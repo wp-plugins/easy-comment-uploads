2c8fd4a527270ba7705b6cceb7ac48cb947a196f
tomdwright <tomdwright@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-10 20:30:51 +0000

    Fix headers
    git-svn-id: https://plugins.svn.wordpress.org/easy-comment-uploads/tags/0.54@250762 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ upload-form.php
@@ -0,0 +1,7 @@
+<!doctype html> 
+<?php require ('../../../wp-blog-header.php'); ?>
+<body style="background : transparent; font-family : 'Lucida Grande', Verdana, Arial, sans-serif; font-size : 10pt">
+	<?php require ('../../../wp-blog-header.php'); 
+		ecu_upload_form_core ();
+	?>
+</body>
